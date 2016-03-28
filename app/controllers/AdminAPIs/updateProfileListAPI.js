6f5a18b38679c523ca6a3aaa4452cbf19800e903
DevZubair <fly_zubair@hotmail.com>
2016-03-28 14:50:11 +0500

    profile list edit

--- app/controllers/AdminAPIs/updateProfileListAPI.js
+++ app/controllers/AdminAPIs/updateProfileListAPI.js
@@ -9,170 +9,14 @@ module.exports = function (app) {
 
 router.post('/updateProfileList', function (req, res, next) {
 
-  var _profileC = req.body.profileCateg,
-    _profileName = req.body.profileItem,
-    profileMake = req.body.profileMake;
-
-  if (_profileC == 'hospitalName') {
-
-    ProfileList.update({}, {
-      $push: {
-        hospitalName: _profileName
-
-      },
-      updated_at: new Date()
-    }, function (err, ProfileData) {
-      if (err) {
-        res.send({
-          code: 500,
-          content: 'Internal Server error',
-          msg: 'API error'
-        });
-      }
-      else if (ProfileData != null) {
-
-        res.send({
-          code: 200,
-          content: 'OK',
-          msg: 'List updated'
-        });
-      }
-    });
-  }
-  else if(_profileC == 'grade'){
-    ProfileList.update({}, {
-      $push: {
-        grade: _profileName
-
-      },
-      updated_at: new Date()
-    }, function (err, ProfileData) {
-      if (err) {
-        res.send({
-          code: 500,
-          content: 'Internal Server error',
-          msg: 'API error'
-        });
-      }
-      else if (ProfileData != null) {
-
-        res.send({
-          code: 200,
-          content: 'OK',
-          msg: 'List updated'
-        });
-      }
-    });
-  }
-  else if(_profileC == 'speciality'){
-    ProfileList.update({}, {
-      $push: {
-        speciality: _profileName
-
-      },
-      updated_at: new Date()
-    }, function (err, ProfileData) {
-      if (err) {
-        res.send({
-          code: 500,
-          content: 'Internal Server error',
-          msg: 'API error'
-        });
-      }
-      else if (ProfileData != null) {
-
-        res.send({
-          code: 200,
-          content: 'OK',
-          msg: 'List updated'
-        });
-      }
-    });
-  }
-  else if(_profileC == 'role'){
-    ProfileList.update({}, {
-      $push: {
-        role: _profileName
-
-      },
-      updated_at: new Date()
-    }, function (err, ProfileData) {
-      if (err) {
-        res.send({
-          code: 500,
-          content: 'Internal Server error',
-          msg: 'API error'
-        });
-      }
-      else if (ProfileData != null) {
-
-        res.send({
-          code: 200,
-          content: 'OK',
-          msg: 'List updated'
-        });
-      }
-    });
-  }
-  else if(_profileC == 'job'){
-    ProfileList.update({}, {
-      $push: {
-        job: _profileName
-
-      },
-      updated_at: new Date()
-    }, function (err, ProfileData) {
-      if (err) {
-        res.send({
-          code: 500,
-          content: 'Internal Server error',
-          msg: 'API error'
-        });
-      }
-      else if (ProfileData != null) {
-
-        res.send({
-          code: 200,
-          content: 'OK',
-          msg: 'List updated'
-        });
-      }
-    });
-  }
-  else if(_profileC == 'ward'){
-    ProfileList.update({}, {
-      $push: {
-        ward: _profileName
-
-      },
-      updated_at: new Date()
-    }, function (err, ProfileData) {
-      if (err) {
-        res.send({
-          code: 500,
-          content: 'Internal Server error',
-          msg: 'API error'
-        });
-      }
-      else if (ProfileData != null) {
-
-        res.send({
-          code: 200,
-          content: 'OK',
-          msg: 'List updated'
-        });
-      }
-    });
-  }
-  else if(profileMake == 1){
     var profile_info = new ProfileList({
 
       hospitalName: [],
-      grade: [],
-      speciality: [],
-      role: [],
+      doctor: [],
+      nurse: [],
+      admin: [],
       job : [],
-      ward : [],
+      professional : [],
       updated_at: new Date()
     });
 
@@ -189,16 +33,9 @@ router.post('/updateProfileList', function (req, res, next) {
         res.send({
           code: 200,
           content: 'OK',
-          msg: 'Profile List is saved'
+          msg: 'Profile List is created'
         });
       }
     });
-  }
-  else{
-    res.send({
-      code: 404,
-      content: 'Category not found',
-      msg: 'Error'
-    });
-  }
+
 });
--- app/models/profileListSchema.js
+++ app/models/profileListSchema.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 var ProfileListSchema = new Schema({
-  hospitalName: [],
-  grade: [],
-  speciality: [],
-  role : [],
   job : [],
-  ward : [],
-  updated_at: {type: Date}
+  hospitalName : [],
+  doctor : [],
+  nurse : [],
+  professional : [],
+  admin : [],
+  updated_at : {type : Date}
 });
 
 mongoose.model('ProfileList', ProfileListSchema);
